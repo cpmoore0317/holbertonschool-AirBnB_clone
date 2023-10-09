dc7c881834019a15a358c743a47360e9c07b538e
mattkrozel <mattkrozel@gmail.com>
2023-10-08 18:54:53 -0700

    added and created cclasses to all files necessary

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''
+define amenity class
+'''
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''
+    represent amentiy class objects
+    '''
+    name = ''
--- models/city.py
+++ models/city.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+'''
+defines city class
+'''
+from models.base_model import BaseModel
 
+
+class City(BaseModel):
+    '''
+    represent city class objects
+    '''
+    state_id = ''
+    name = ''
