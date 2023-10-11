8c97a6f456f8f55e51afacf76a3c933935ffcdce
“cpmoore0317” <kingpkayy@gmail.com>
2023-10-10 21:48:28 -0700

    Filestorage updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,11 @@
 """ Module for FileStorage class. """
 import json
 from models.base_model import BaseModel
-
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """
@@ -25,22 +29,46 @@ class FileStorage:
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
+    def classes(self):
+        """
+        Return a dictionary of all the supported classes for serialization.
+        You can add more classes as needed.
+        """
+        return {
+            'BaseModel': BaseModel,
+            'Place': Place,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Review': Review
+        }
+
     def save(self):
-        """ Serializes __objects to the JSON file (path: __file_path) """
+        """
+        Serializes __objects to the JSON file (path: __file_path).
+        This includes all the supported classes.
+        """
         dict_obj = FileStorage.__objects
-        serialized_dict = {obj: dict_obj[obj].to_dict() for obj in dict_obj.keys()}
+        serialized_dict = {
+            key: obj.to_dict()
+            for key, obj in dict_obj.items()
+        }
         with open(FileStorage.__file_path, "w") as json_file:
             json.dump(serialized_dict, json_file)
 
     def reload(self):
-        """ Deserializes the JSON file to __objects if the JSON file
-        exists ; otherwise, nothing. """
+        """
+        Deserializes the JSON file to __objects if the JSON file exists.
+        This handles all the supported classes.
+        """
         try:
             with open(FileStorage.__file_path, "r") as json_file:
                 dict_obj = json.load(json_file)
-                for value in dict_obj.values():
+                for key, value in dict_obj.items():
                     class_name = value["__class__"]
                     del value["__class__"]
-                    self.new(eval(class_name)(**value))
+                    obj_class = self.classes().get(class_name)
+                    if obj_class:
+                        self.new(obj_class(**value))
         except FileNotFoundError:
             pass
